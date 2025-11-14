3a5008f932ac111d8f850dd5cb05659be1ed6748
Erika Mesh <esmvcs@rit.edu>
2025-11-14 10:01:51 -0500

    HW 5 starter

---
+++ HW5_TheFarmstead/Program.cs
@@ -0,0 +1,23 @@
+﻿/***
+ * YOUR NAME
+ * 
+ * HW 5 - The Farmstead
+ * Assignment Write-Up: https://docs.google.com/document/d/1lHcOHZfS4NGsVFBPYU7IA6HgXgWUapI5OKKlDHN3fuc/edit?usp=sharing
+ * GitHub repo URL:
+ * 
+ */
+
+/// ALL of your source code should follow all IGME-105 course coding standards.
+/// https://people.rit.edu/edcigm/gdaps/CodingStandards.html
+/// 
+/// Do NOT use any additional classes or other techniques 
+/// that have not been covered in class.
+namespace HW5_TheFarmstead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
